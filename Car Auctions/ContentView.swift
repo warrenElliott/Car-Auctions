af05ae0c0c3358460af03eee311a90124bd41ce0
warrenElliott <warren.elliott1995@gmail.com>
2020-06-17 11:02:51 +0100

    Initial Commit

---
+++ Car Auctions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Car Auctions
+//
+//  Created by Warren Elliott on 17/06/2020.
+//  Copyright © 2020 Warren Elliott. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
